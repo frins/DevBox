837e587beae24880cc01e782931ac19318d19cdf
Francesc <francesc.bgr@gmail.com>
2010-09-18 18:02:17 +0200

    Fixed a Syntax Error

--- config.php
+++ config.php
@@ -7,4 +7,4 @@ define('DB_TABLE', 'devbox');
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWD)
     or die('Could not connect: ' . mysql_error());
 mysql_select_db(DB_TABLE) or die('Could not select database');
-define('INC_PATH', $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'includes'. . DIRECTORY_SEPARATOR .);
+define('INC_PATH', $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR);
