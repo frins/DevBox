dbe8e1267cda61fa177b9d222b28a74c08b782b0
Francesc <francesc.bgr@gmail.com>
2010-09-25 10:03:59 +0200

    Home Page for registered Users

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+include('config.php');
+include('includes/main.php');
+/*
+ * Templates
+ */
+include('templates/header.php');
+include('templates/home.php');
+include('templates/footer.php');
