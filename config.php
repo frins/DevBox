56226a4da99c2523d17d1f4b54c7d68c06507582
Francesc <francesc.bgr@gmail.com>
2010-09-25 09:52:25 +0200

    Updated DB config

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWD', '');
+define('DB_PASSWD', 'root');
 define('DB_TABLE', 'devbox');
-
+/**
+ * Configuration
+ */
+$config = '';
+$config['site_title'] = 'Site Title';
 $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWD)
     or die('Could not connect: ' . mysql_error());
 mysql_select_db(DB_TABLE) or die('Could not select database');
